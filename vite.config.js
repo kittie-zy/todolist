63e9e22d0aa0c6615a25805009c52e06929a988b
Minjin <kittiezy@gmail.com>
2026-04-15 10:44:48 +0900

    깃헙액션에 디플로이, 환경설정

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from "path";
+import { fileURLToPath } from "url";
+
+// https://vite.dev/config/
+// ESM 환경에서 __dirname 객체를 생성하기 위한 코드
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 // https://vite.dev/config/
 export default defineConfig({
